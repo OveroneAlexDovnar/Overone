59ae9cebdebb0c4bbed3fb20c01fb6f091eb013f
Alex Dovnar <alexdovnar@yahoo.com>
2020-11-03 20:08:05 +0300

    '12'

--- teachers.py
+++ teachers.py
@@ -1,6 +1,10 @@
-# import teachers.Teachers from teachers.py
+from school.human import Human
 
-def list_of_teachers():
-    list_of_teachers = []
-    list_of_teachers.append(self, teachers)
-    print(list_of_teachers)
+
+class Teacher(Human):
+    def __init__(self, name, last_name, age, work_experience):
+        super().__init__(name, last_name, age)
+        self.__work_experience = work_experience
+
+    def __str__(self):
+        return f'name: {self._name}, last name: {self._last_name}, age: {self._age}, work experience: {self.__work_experience}'
