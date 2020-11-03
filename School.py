3e810fd81910d93ff3a5279ac5e199c371d8f801
Alex Dovnar <alexdovnar@yahoo.com>
2020-11-03 20:09:15 +0300

    'stud'

---
+++ School.py
@@ -0,0 +1,26 @@
+class School:
+    def __init__(self, name, groups: list, personals: list):
+        self.__name = name
+        self.__groups = groups
+        self.__personas = personals
+        self._teachers = []
+        self._group_list = []
+        self._group_students = []
+
+    def __str__(self):
+        return f'{self.__name}'
+
+    def add_group(self, number):
+        group_list.append(self, "number")
+
+    def group_delete(self, number):
+        for i in group_list:
+            if True:
+                group_list.__delitem__(self, number(i))
+
+
+    def number_of_groups(self, number_of_groups: list):
+        return len([group_students])
+
+    def teachers_of_the_school(self, name, last_name):
+        for i in group_list:
